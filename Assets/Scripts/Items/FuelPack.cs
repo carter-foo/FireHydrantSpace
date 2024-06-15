b788d00e3787dc1f0b944ee762fb26a1b60e51ec
Kiydo <e.magtoto@hotmail.com>
2024-06-15 13:39:54 -0600

    camera holder changes

--- Assets/Scripts/Items/FuelPack.cs
+++ Assets/Scripts/Items/FuelPack.cs
@@ -9,16 +9,27 @@ public class FuelPack : MonoBehaviour
 
     public float refillAmount = 25f;
 
+    public float floatSpeed = 0.5f;
+    public float floatHeight = 0.5f;
+    private Vector3 startPos;
+
+    public float rotateSpeed = 90f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Floating effect
+        float newY = startPos.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+        // Rotating effect
+        transform.Rotate(Vector3.right * rotateSpeed * Time.deltaTime);
     }
 
     void OnTriggerEnter(Collider other)
