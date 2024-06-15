65b89a13e0b18a76648c60ff5c78727f38cbdaaf
Kiydo <e.magtoto@hotmail.com>
2024-06-14 18:19:39 -0600

    Added Extinguisher Thrust Movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float thrust = 10.0f;
+    public float decelerationRate = 0.95f;
     private Rigidbody rb;
+    public Transform cameraTransform;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     // Update is called once per frame
@@ -19,12 +22,21 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetMouseButton(1))
         {
             ApplyThrust();
+        } 
+        else
+        {
+            Decelerate();
         }
     }
 
     void ApplyThrust()
     {
-        Vector3 forceDirection = -transform.forward;
+        Vector3 forceDirection = -cameraTransform.forward; // Thrust Backwards
         rb.AddForce(forceDirection * thrust, ForceMode.Acceleration);
+    }
+
+    void Decelerate() 
+    {
+        rb.velocity *= decelerationRate;
     }
 }
